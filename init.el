edee0ce7d77910f939c09b9da80abfccc81a4414
Jan Pochyla <jpochyla@gmail.com>
2014-10-07 15:13:07 +0200

    Fix enter in multiple cursors

--- init.el
+++ init.el
@@ -299,6 +299,7 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C->") 'mc/mark-all-like-this)
+(define-key mc/keymap (kbd "<return>") nil)
 
 ;; Moving line/region up/down
 (require 'shift-text)
